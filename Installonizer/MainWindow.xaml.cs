041e4524b34b0479aba2fc9b7e3a8d298957cdd3
Benjamin <benjamin.goisser.bg@gmail.com>
2019-09-09 21:15:57 +0200

    Moving Folders to Program Files
    Folders can now be moved to Program Files or Program Files (x86).
    Also implemented administrator permission request and 64-bit platform target.
    0.0.2

--- Installonizer/MainWindow.xaml.cs
+++ Installonizer/MainWindow.xaml.cs
@@ -12,49 +12,103 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Path = System.IO.Path;
 
 namespace Installonizer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+	public partial class MainWindow : Window
+	{
+		FileType setFileType;
+		string filename;
 
-        private void BtSetPath_Click(object sender, RoutedEventArgs e)
-        {
-            if ((bool)!cbBrowseFile.IsChecked)
-            {
-                System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog
-                {
-                    Description = "Select Folder to be installonized:",
-                    ShowNewFolderButton = false,
-                    RootFolder = Environment.SpecialFolder.UserProfile
-                };
-                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-                {
-                    tbPath.Text = dialog.SelectedPath;
-                }
-                dialog.Dispose();
-            }
-            else
-            {
-                System.Windows.Forms.OpenFileDialog dialog = new System.Windows.Forms.OpenFileDialog()
-                {
-                    Title = "Select File to be installonized",
-                    Filter = "Executables and Batch Files (*.exe, *.bat) | *exe; .bat | All Files (*.*) | *.*"
-                };
-                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-                {
-                    tbPath.Text = dialog.FileName;
-                }
-                dialog.Dispose();
-            }
-            
-        }
-    }
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private enum FileType
+		{
+			Executable,
+			Directory,
+			NotSet
+		}
+
+		private void BtSetPath_Click(object sender, RoutedEventArgs e)
+		{
+			if ((bool)!cbBrowseFile.IsChecked)
+			{
+				System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog
+				{
+					Description = "Select Folder to be installonized:",
+					ShowNewFolderButton = false,
+					RootFolder = Environment.SpecialFolder.UserProfile
+				};
+
+				if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				{
+					filename = Path.GetFileNameWithoutExtension(dialog.SelectedPath);
+					tbPath.Text = dialog.SelectedPath;
+					setFileType = FileType.Directory;
+				}
+				dialog.Dispose();
+			}
+			else
+			{
+				System.Windows.Forms.OpenFileDialog dialog = new System.Windows.Forms.OpenFileDialog()
+				{
+					Title = "Select File to be installonized",
+					Filter = "Executables and Batch Files (*.exe, *.bat) | *exe; .bat | All Files (*.*) | *.*"
+				};
+
+				if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				{
+					filename = Path.GetFileNameWithoutExtension(dialog.FileName);
+					tbPath.Text = dialog.FileName;
+					setFileType = FileType.Executable;
+				}
+				dialog.Dispose();
+			}
+
+		}
+
+		private void TbPath_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			setFileType = FileType.NotSet;
+		}
+
+		private void BtInstallonize_Click(object sender, RoutedEventArgs e)
+		{
+			if (setFileType != FileType.NotSet)
+			{
+				if (setFileType == FileType.Directory)
+				{
+					if ((bool)!cbX86.IsChecked)
+					{
+						try
+						{
+							MessageBox.Show(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + "\\" + filename);
+							Directory.Move(tbPath.Text, Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + "\\" + filename);
+						}
+						catch
+						{
+
+						}
+					}
+					else
+					{
+						try
+						{
+							MessageBox.Show(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + "\\" + filename);
+							Directory.Move(tbPath.Text, Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + "\\" + filename);
+						}
+						catch
+						{
+
+						}
+					}
+				}
+			}
+		}
+	}
 }
